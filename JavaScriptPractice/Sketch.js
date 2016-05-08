daa97ce7eafc2a67b23d2cb788042cf835d8fbf0
Gen Fillipow <Gen Fillipow>
2016-05-08 14:46:40 -0700

    Uploading my simple sketching code

---
+++ JavaScriptPractice/Sketch.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+   function createBoard(size){
+      var squareSize = 500/size;
+      for (var j = 0; j < size; j++){
+         var row = "<div>";
+         for (var i = 0; i < size; i++){
+            row += "<div class='square'></div>";
+         }
+         row += "</div>"
+         $("#container").append(row);
+      }
+      $(".square").css({"height": squareSize, "width": squareSize})
+   };
+
+   function changeColor(){
+      $(".square").hover(function(){
+         $(this).css("background-color", "white");
+      });
+   };
+
+   createBoard(16);
+   changeColor();
+   
+   $("button").click(function(){
+      var size = prompt("Please enter the size of board");
+      $("#container").empty();
+      createBoard(size);
+      changeColor();
+   });
+});
